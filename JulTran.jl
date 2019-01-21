29a917e4249180b770c238ad35c74a88b03c1da0
Yunlong Lian <algorithmx@users.noreply.github.com>
2019-01-20 22:19:18 -0500

    Update JulTran.jl

--- JulTran.jl
+++ JulTran.jl
@@ -12,32 +12,41 @@ macro Ndeclare(A, T)
     return :( $(esc(A)) = NONALLOCATO($(esc(T))) )
 end
 
-macro Ninit(A, T, V)
-    return :( $(esc(A)) = convert($(esc(T)),$(esc(V))) )
+#https://stackoverflow.com/questions/39931112/quote-unquote-idiom-in-julia-concatenating-expr-objects
+import Base.zero
+zero(::Type{T}) where{T<:AbstractString} = string("")
+
+macro declareNtimes(vars...)
+    ex = Expr(:block)
+    t = vars[1]
+    ex.args = [:($(esc(n)) = Base.zero($(esc(t)))) for n in vars[2:end]]
+    ex
 end
 
+#@declareNtimes Bool a b c
+
+macro Ninit(A, T, V)
+    return :( $(esc(A)) = Base.convert($(esc(T)),$(esc(V))) )
+end
+
+macro NinitNtimes(vars...)
+    ex = Expr(:block)
+    t = vars[1]
+    ex.args = [:( $(esc(c.args[1])) = Base.convert($(esc(t)), $(esc(c.args[2]))) ) for c in vars[2:end]]
+    ex
+end
+
+#@NinitNtimes Bool (a,false) (b,true)
+#@assert !a && b
+
 macro allocate(A, T, S)
-    return :( $(esc(A)) = fill(zero($(esc(T))),$(esc(S))) )
+    return :( $(esc(A)) = Base.fill(Base.zero($(esc(T))),$(esc(S))) )
 end
 
 macro deallocate(a)
-    return :( $(esc(a)) = NONALLOCATO(typeof($(esc(a)))) )
+    return :( $(esc(a)) = NONALLOCATO(Base.typeof($(esc(a)))) )
 end
 
 macro isallocated(a)
-    return :( ! isa($(esc(a)), NONALLOCATO) )
+    return :( ! Base.isa($(esc(a)), NONALLOCATO) )
 end
-
-#@isallocated A
-
-#@declare A Float64 (2,3,4)
-
-#@allocate A Float64 (2,3,4)
-
-#@deallocate A
-
-#@macroexpand (@deallocate A)
-
-#A = rand(20000,20000)
-
-#@deallocate A
