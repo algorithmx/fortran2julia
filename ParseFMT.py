93c8d10244c757eadfb85d9d82c2809b2f2b2c9b
Yunlong Lian <algorithmx@users.noreply.github.com>
2019-02-06 13:43:11 -0500

    Update ParseFMT.py

--- ParseFMT.py
+++ ParseFMT.py
@@ -1,4 +1,5 @@
 import re
+from Utinity import split_on_toplevel_comma, split_on_slash
 
 def prt_string(fmt):
     return ("%s" if len(fmt)==1 else ('%-'+(fmt[1:])+'s'))
@@ -20,17 +21,24 @@ def prt_int(fmt):
     return ('%'+fmt[1:]+'i')
 
 
-def float_ord_format_spec(fmt):
+def float_____format_spec(fmt,spec):
+    F = spec.upper()
+    f = spec.lower()
     # matched r"\d*[fF]\d+\.\d+"
-    return ( ('%'+fmt[1:]+'f') \
-            if (fmt[0]=='f' or fmt[0]=='F') \
-            else (('%'+fmt.lower().split('f')[1]+'f')*int(fmt.lower().split('f')[0])) )
+    return ( ('%'+fmt[1:]+ f) \
+            if (fmt[0]==f or fmt[0]==F) \
+            else ( ('%'+fmt.lower().split(f)[1]+f)*int(fmt.lower().split(f)[0])) )
+
+def float_ord_format_spec(fmt):
+    return float_____format_spec(fmt,'f')
 
 
 def float_sci_format_spec(fmt):
-    return ( ('%'+fmt[1:]+'e') \
-            if (fmt[0]=='e' or fmt[0]=='E') \
-            else (('%'+fmt.lower().split('e')[1]+'e')*int(fmt.lower().split('e')[0])) )
+    return float_____format_spec(fmt,'e')
+
+
+def float_ggg_format_spec(fmt):
+    return float_____format_spec(fmt,'g')
 
 
 def no_spec(fmt):
@@ -38,7 +46,8 @@ def no_spec(fmt):
 
 
 def new_line(fmt):
-    return '\\n'
+    print("[INFO:NEW_LINE] " + str(len(fmt)))
+    return '\\n' * len(fmt)
 
 
 special_chars = [ (r"\$",'\\$'), (r"\\",'\\\\'), (r"\"",'\\\"'), (r"%","%%") ]
@@ -59,8 +68,9 @@ spec_regex = [  re.compile(r"([\"\'])((\\\\{2})*|(.*?[^\\](\\{2})*))\1"),
                 re.compile(r"[iI]\d+"),
                 re.compile(r"\d*[fF]\d+\.\d+"),
                 re.compile(r"\d*[eE]\d+\.\d+"),
+                re.compile(r"\d*[gG]\d+\.\d+"),
                 re.compile(r"\*"),
-                re.compile(r"\/"),
+                re.compile(r"[\/]+"),
                 re.compile(r"[aA]\d*") ]
 
 
@@ -71,42 +81,35 @@ spec_trans = {  spec_regex[0]: iden_string, \
                 spec_regex[3]: prt_int, \
                 spec_regex[4]: float_ord_format_spec, \
                 spec_regex[5]: float_sci_format_spec, \
-                spec_regex[6]: no_spec, \
-                spec_regex[7]: new_line, \
-                spec_regex[8]: prt_string }
-
+                spec_regex[6]: float_ggg_format_spec, \
+                spec_regex[7]: no_spec, \
+                spec_regex[8]: new_line, \
+                spec_regex[9]: prt_string }
 
 
 
 def parse_fmt(s):
     trans = []
-    for spec0 in s.strip(' ()').strip(' ()').split(','):
-        spec = spec0.strip()
-        if spec=='/':
-            trans.append('\\n')
+    spec_list = split_on_toplevel_comma(s.strip(' ()').strip(' ()'))
+    for spec0 in spec_list:
+        l,c,r = split_on_slash(spec0.strip())
+        if (len(c) == 0) and (len(r) == 0):
+            trans.append("\\n"*len(l))
             continue
-        else:
-            begin_slash = spec[0]=='/'
-            end_slash = spec[-1]=='/'
-            spec = (spec[1:] if begin_slash else spec0)
-            spec = (spec[:-1] if end_slash else spec)
-            for reg in spec_regex:
-                if reg.search(spec) is not None:
-                    spec_tr = spec_trans[reg](spec.strip())
-                    spec_tr = ('\\n'+spec_tr if begin_slash else spec_tr)
-                    spec_tr = (spec_tr+'\\n' if end_slash else spec_tr)
-                    trans.append(spec_tr)
-                    break
-                #end #if
-            #end #for
-        #end #if
+
+        for reg in spec_regex:
+            if reg.search(c) is not None:
+                spec_tr = spec_trans[reg](c.strip())
+                trans.append( ("\\n"*len(l)) + spec_tr + ("\\n"*len(r)) )
+                break
+            #end #if
+        #end #for
     #end #for
     return (''.join(trans))
 
 
-
-
-
+#sss = "(7g18.10)"
+#parse_fmt(sss)
 #parse_fmt("(A3,'\" ',F8.5,') ')")
 #sss = "('set style data dots',/,'unset key',/, 'set xrange [0:',F8.5,']',/,'set yrange [',F16.8,' :',F16.8,']')"
 #print(parse_fmt(sss))
